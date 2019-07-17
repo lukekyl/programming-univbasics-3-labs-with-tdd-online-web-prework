442a8401426c6f16f187e38fdb76f7e08dc0ec1c
Kyle Luke <th.oldschool@gmail.com>
2019-07-17 00:40:41 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -6,8 +6,8 @@ second_number = 5
 
 sum = first_number + second_number
 
-difference = first_number-second_number
+difference = first_number - second_number
 
 product = first_number * second_number
 
-difference = first_number / second_number
+quotient = first_number / second_number
