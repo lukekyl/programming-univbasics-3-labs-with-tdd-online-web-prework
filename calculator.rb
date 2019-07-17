f97ce7ec9d915d9e76fc3a5b00c10400b1474799
Kyle Luke <th.oldschool@gmail.com>
2019-07-17 00:39:32 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,11 @@
 first_number = 2 
 
 second_number = 5
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+difference = first_number / second_number
